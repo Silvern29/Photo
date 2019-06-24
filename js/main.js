0da2fb1704e03bf766d6f1ccc7bcd26b752e29fd
Florian Redlinghaus <florian@redlinghaus.at>
2019-06-24 21:58:43 +0200

    fixed reload on media query breakpoint in js

--- js/main.js
+++ js/main.js
@@ -29,13 +29,47 @@ function splitScroll(){
     .addTo(controller);
 }
 
-
-
-window.onresize = function() {
-  var myUrl = window.location;
-  window.location.replace(myUrl)
-}
-
 if (document.documentElement.clientWidth > 822) {
   splitScroll();
 }
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+var cStatus;
+// var cWidth = document.documentElement.clientWidth;
+// var $window = $(window)
+
+if (document.documentElement.clientWidth >= 822) {
+  cStatus = 'large';
+} else if (document.documentElement.clientWidth < 822) {
+  cStatus = 'small';
+}
+
+let reload = () => {
+  var myUrl = window.location;
+  window.location.replace(myUrl);
+}
+
+window.onresize = function() {
+  if ((cStatus != 'small' && document.documentElement.clientWidth < 822) || (cStatus != 'large' && document.documentElement.clientWidth >= 822)) {
+    reload();
+  }
+}
+
+// const mq = window.matchMedia( "(min-width: 840px)" );
+// window.onresize = function() {
+//   if (mq.matches) {
+//     splitScroll();
+//   }
+// }
+
